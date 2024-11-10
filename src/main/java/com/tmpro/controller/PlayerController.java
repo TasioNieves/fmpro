9016131279c4a3337b6f851c5646d3704ee301a3
tasioo muñoz <amn_14@hotmail.es>
2024-11-10 02:59:57 +0100

    Renombrar procfile a Procfile

--- src/main/java/com/tmpro/controller/PlayerController.java
+++ src/main/java/com/tmpro/controller/PlayerController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/players")
+@RequestMapping("/players")
 public class PlayerController {
 
     @Autowired
