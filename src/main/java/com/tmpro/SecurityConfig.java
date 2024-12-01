378e2eb32b16d879dc508f066ee8eba6cf1b6109
tasioo muñoz <amn_14@hotmail.es>
2024-12-01 13:35:13 +0100

    Resolver conflictos de sincronización

--- src/main/java/com/tmpro/SecurityConfig.java
+++ src/main/java/com/tmpro/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // Desactiva CSRF si no usas tokens
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/index.html", "/api/auth/**").permitAll() // Permite acceso público a estas rutas
+                        .requestMatchers("/", "/index.html", "/api/auth/**", "/static/**").permitAll() // Permite acceso público a estas rutas
                         .anyRequest().authenticated() // Requiere autenticación para otras rutas
                 )
                 .formLogin(form -> form.disable()); // Desactiva el formulario de login predeterminado
