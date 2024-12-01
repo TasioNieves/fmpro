12041f2247b3be15d8f2dac7be4bdbda9b51db4f
tasioo muñoz <amn_14@hotmail.es>
2024-12-01 21:17:23 +0100

    Renombrar procfile a Procfile

--- src/main/java/com/tmpro/SecurityConfig.java
+++ src/main/java/com/tmpro/SecurityConfig.java
@@ -7,25 +7,52 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // Desactiva CSRF si no usas tokens
-
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configurar CORS
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/api/roles/", "/index.html", "/api/**", "/static/**", "/styles.css", "/runtime.js", "/polyfills.js", "/vendor.js", "/main.js", "/favicon.ico").permitAll() // Permite acceso público a estas rutas
-
-
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .formLogin(AbstractHttpConfigurer::disable); // Desactiva el formulario de login predeterminado
-
+                .csrf(AbstractHttpConfigurer::disable); // Deshabilitar CSRF si es necesario
 
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://tmpro-5265168c0988.herokuapp.com/")); // Cambia según tus necesidades
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+
+
+
+
+
+
+
 
 
 
