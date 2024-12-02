03a198d7b946fe74d81126b2a4bf540088f56662
tasioo muñoz <amn_14@hotmail.es>
2024-12-02 16:12:09 +0100

    Renombrar procfile a Procfile

--- src/main/java/com/tmpro/controller/AuthController.java
+++ src/main/java/com/tmpro/controller/AuthController.java
@@ -44,26 +44,15 @@ public class AuthController {
      * Endpoint para autenticar a un usuario.
      *
      * @param user Contiene el nombre de usuario y la contraseña.
-     * @return El usuario autenticado con su rol.
+     * @return El usuario autenticado.
      */
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User user) {
         try {
-            // Intentar autenticar al usuario con su nombre de usuario y contraseña.
             User authenticatedUser = authService.login(user.getUsername(), user.getPassword());
-
-            // Verificar si el usuario tiene un rol asignado y agregar esa información a la respuesta.
-            if (authenticatedUser.getRole() != null) {
-                // Devolver la información del usuario con el rol
-                return ResponseEntity.ok(authenticatedUser);
-            } else {
-                // Si no tiene un rol, enviar una respuesta de error
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario no tiene un rol asignado");
-            }
+            return ResponseEntity.ok(authenticatedUser);
         } catch (RuntimeException e) {
-            // Si ocurre un error durante la autenticación, devolver un mensaje de error
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
 }
